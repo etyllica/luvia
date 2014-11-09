e75be9ef625c708d3e6c435cafa965a981b9162e
yuripourre <yuriamayuri@gmail.com>
2014-11-09 00:57:57 -0200

    Improves Mesh Loading

--- Luvia/src/br/com/luvia/loader/mesh/MeshLoader.java
+++ Luvia/src/br/com/luvia/loader/mesh/MeshLoader.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import br.com.etyllica.core.loader.LoaderImpl;
 import br.com.luvia.linear.Mesh;
@@ -19,6 +21,8 @@ public class MeshLoader extends LoaderImpl {
 
 	private static MeshLoader instance = null;
 
+	private Map<String, Mesh> meshes = new HashMap<String, Mesh>();
+
 	public static MeshLoader getInstance() {
 		if(instance==null){
 			instance = new MeshLoader();
@@ -26,34 +30,42 @@ public class MeshLoader extends LoaderImpl {
 
 		return instance;
 	}
-	
+
 	private MeshLoader() {
 		super();
-		
+
 		folder = "assets/models/";
 	}
-	
+
 	public Mesh loadModel(String path) {
-		
+
 		String diretorio = folder+path;
-		
+
 		URL dir = null;
 		try {
 			dir = new URL(url, diretorio);
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 		}
+
+		Mesh mesh = meshes.get(diretorio);
 		
-		try {
-			return OBJLoader.loadModel(dir);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+		if(mesh == null) {
+
+			try {
+				mesh = OBJLoader.loadModel(dir);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			meshes.put(diretorio, mesh);
 		}
 		
-		return null;
-		
+		return mesh;
 	}
-	
+
 }
