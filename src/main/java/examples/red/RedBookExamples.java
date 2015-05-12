9f31ede1149b8d30deff765d68ce3c2b9023c2c7
Yuri Pourre <yuriamayuri@gmail.com>
2015-05-12 18:01:29 -0300

    Add the example 1 of the red book

---
+++ src/main/java/examples/red/RedBookExamples.java
@@ -0,0 +1,27 @@
+package examples.red;
+
+import br.com.luvia.Luvia;
+import br.com.luvia.core.context.ApplicationGL;
+
+public class RedBookExamples extends Luvia {
+
+	public RedBookExamples() {
+		super(1024,576);
+	}
+
+	// Main program
+	public static void main(String[] args) {
+
+		RedBookExamples examples = new RedBookExamples();		
+		examples.init();
+	}
+	
+	@Override
+	public ApplicationGL startApplication() {
+
+		initialSetup("../../../../");
+
+		return new Example1(w, h);
+	}
+
+}
