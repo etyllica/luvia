cc949a3fefb44f373fe059e05bb1a89c25bf6c1e
Yuri Pourre <yuriamayuri@gmail.com>
2015-05-15 19:10:25 -0300

    Improves Sphere

--- src/main/java/br/com/luvia/geom/Sphere.java
+++ src/main/java/br/com/luvia/geom/Sphere.java
@@ -1,8 +1,6 @@
 package br.com.luvia.geom;
 
-import javax.media.opengl.GL2;
-import javax.media.opengl.glu.GLU;
-import javax.media.opengl.glu.GLUquadric;
+import br.com.luvia.core.video.Graphics3D;
 
 public class Sphere extends GeometricForm {
 
@@ -10,45 +8,13 @@ public class Sphere extends GeometricForm {
 	
 	private double radius = 1;
 	
-	private int slices = DEFAULT_RESOLUTION;
-	private int stacks = DEFAULT_RESOLUTION;
-
 	public Sphere(double radius) {
 		super();		
 		this.radius = radius;
 	}
 	
-	//TODO avoid GLU usage
-	public void draw(GL2 gl, GLU glu) {
-
-		gl.glPushMatrix();
-	
-		changeColor(gl);
-
-		gl.glTranslated(x, y, z);
-
-		drawSphere(glu);
-
-		gl.glPopMatrix();
-	}
-
-	protected void drawSphere(GLU glu) {
-		GLUquadric sphere = generateSphereQuadric(glu);
-
-		glu.gluSphere(sphere, radius, slices, stacks);
-
-		glu.gluDeleteQuadric(sphere);
-	}
-	
-	private GLUquadric generateSphereQuadric(GLU glu) {
-		GLUquadric sphere = glu.gluNewQuadric();
-
-		// Draw sphere (possible styles: FILL, LINE, POINT)
-		glu.gluQuadricDrawStyle(sphere, GLU.GLU_FILL);
-		glu.gluQuadricNormals(sphere, GLU.GLU_FLAT);
-		glu.gluQuadricOrientation(sphere, GLU.GLU_OUTSIDE);
-		
-		return sphere;
+	public void draw(Graphics3D g) {
+	  g.drawSphere(radius, x, y, z, DEFAULT_RESOLUTION, color);	  
 	}
 	
 }
