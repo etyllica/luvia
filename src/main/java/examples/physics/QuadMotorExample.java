fc20f11c37f6c39c10ad84e048dc0e814033f5a4
Yuri Pourre <yuriamayuri@gmail.com>
2015-06-17 17:36:08 -0400

    Tries to add QuadMotorExample

---
+++ src/main/java/examples/physics/QuadMotorExample.java
@@ -0,0 +1,253 @@
+package examples.physics;
+/*
+ * Based on BasicDemo example
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.media.opengl.GL2;
+import javax.vecmath.Vector3f;
+
+import br.com.etyllica.core.event.GUIEvent;
+import br.com.etyllica.core.event.KeyEvent;
+import br.com.luvia.core.video.Graphics3D;
+
+import com.bulletphysics.collision.broadphase.BroadphaseInterface;
+import com.bulletphysics.collision.broadphase.DbvtBroadphase;
+import com.bulletphysics.collision.dispatch.CollisionDispatcher;
+import com.bulletphysics.collision.dispatch.DefaultCollisionConfiguration;
+import com.bulletphysics.collision.shapes.BoxShape;
+import com.bulletphysics.collision.shapes.CollisionShape;
+import com.bulletphysics.collision.shapes.SphereShape;
+import com.bulletphysics.dynamics.DiscreteDynamicsWorld;
+import com.bulletphysics.dynamics.RigidBody;
+import com.bulletphysics.dynamics.RigidBodyConstructionInfo;
+import com.bulletphysics.dynamics.constraintsolver.ConstraintSolver;
+import com.bulletphysics.dynamics.constraintsolver.Point2PointConstraint;
+import com.bulletphysics.dynamics.constraintsolver.SequentialImpulseConstraintSolver;
+import com.bulletphysics.dynamics.constraintsolver.TypedConstraint;
+import com.bulletphysics.linearmath.DefaultMotionState;
+import com.bulletphysics.linearmath.Transform;
+import com.bulletphysics.util.ObjectArrayList;
+
+import examples.jbullet.opengl.DemoApplication;
+
+public class QuadMotorExample extends DemoApplication {
+
+	// create 125 (5x5x5) dynamic object
+	private static final int ARRAY_SIZE_X = 5;
+	private static final int ARRAY_SIZE_Y = 5;
+	private static final int ARRAY_SIZE_Z = 5;
+
+	// maximum number of objects (and allow user to shoot additional boxes)
+	private static final int MAX_PROXIES = (ARRAY_SIZE_X*ARRAY_SIZE_Y*ARRAY_SIZE_Z + 1024);
+
+	private static final int START_POS_X = -5;
+	private static final int START_POS_Y = -5;
+	private static final int START_POS_Z = -3;
+
+	// keep the collision shapes, for deletion/cleanup
+	private ObjectArrayList<CollisionShape> collisionShapes = new ObjectArrayList<CollisionShape>();
+	private BroadphaseInterface broadphase;
+	private CollisionDispatcher dispatcher;
+	private ConstraintSolver solver;
+	private DefaultCollisionConfiguration collisionConfiguration;
+
+	private RigidBody selectedBody;
+
+	boolean pressed = false;
+
+	public QuadMotorExample(int w, int h) {
+		super(w, h);
+
+		setCameraDistance(50f);
+	}
+
+	@Override
+	public void clientMoveAndDisplay() {
+
+		// simple dynamics world doesn't handle fixed-time-stepping
+		float ms = getDeltaTimeMicroseconds();
+
+		// step the simulation
+		if (dynamicsWorld != null) {
+			dynamicsWorld.stepSimulation(ms / 1000000f);
+			// optional but useful: debug drawing
+			dynamicsWorld.debugDrawWorld();
+		}
+	}
+
+	@Override
+	public void display(Graphics3D g) {
+		GL2 gl = g.getGL2();
+
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+
+		super.display(g);
+
+		// optional but useful: debug drawing to detect problems
+		if (dynamicsWorld != null) {
+			dynamicsWorld.debugDrawWorld();
+		}
+
+		if(pressed) {
+			selectedBody.applyCentralImpulse(new Vector3f(0, 10, 0));
+		}
+
+		gl.glFlush();
+	}
+
+	public void initPhysics(Graphics3D g) {
+
+		// collision configuration contains default setup for memory, collision setup
+		collisionConfiguration = new DefaultCollisionConfiguration();
+
+		// use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+		dispatcher = new CollisionDispatcher(collisionConfiguration);
+
+		broadphase = new DbvtBroadphase();
+
+		// the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+		SequentialImpulseConstraintSolver sol = new SequentialImpulseConstraintSolver();
+		solver = sol;
+
+		// TODO: needed for SimpleDynamicsWorld
+		//sol.setSolverMode(sol.getSolverMode() & ~SolverMode.SOLVER_CACHE_FRIENDLY.getMask());
+
+		dynamicsWorld = new DiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+
+		dynamicsWorld.setGravity(new Vector3f(0f, -10f, 0f));
+
+		// create a few basic rigid bodies
+		CollisionShape groundShape = new BoxShape(new Vector3f(50f, 50f, 50f));
+		//CollisionShape groundShape = new StaticPlaneShape(new Vector3f(0, 1, 0), 50);
+
+		collisionShapes.add(groundShape);
+
+		Transform groundTransform = new Transform();
+		groundTransform.setIdentity();
+		groundTransform.origin.set(0, -56, 0);
+
+		createGround(groundShape, groundTransform);
+
+		createMiniCubes();
+
+		clientResetScene();
+	}
+
+	private void createMiniCubes() {
+		// create a few dynamic rigidbodies
+		// Re-using the same collision is better for memory usage and performance
+
+		//CollisionShape colShape = new BoxShape(new Vector3f(1, 1, 1));
+		CollisionShape colShape = new SphereShape(1f);
+		collisionShapes.add(colShape);
+
+		// Create Dynamic Objects
+		Transform startTransform = new Transform();
+		startTransform.setIdentity();
+
+		float mass = 10f;
+
+		// rigidbody is dynamic if and only if mass is non zero, otherwise static
+		boolean isDynamic = (mass != 0f);
+
+		Vector3f localInertia = new Vector3f(0, 0, 0);
+		if (isDynamic) {
+			colShape.calculateLocalInertia(mass, localInertia);
+		}
+
+		float start_x = START_POS_X;
+		float start_y = START_POS_Y;
+		float start_z = START_POS_Z;
+
+		List<RigidBody> bodies = new ArrayList<RigidBody>();
+		
+		for(int i=0;i<2;i++) {
+			bodies.add(createBody(start_x+5*i, start_y, start_z));
+		}
+		
+		/*RigidBody rbA = bodies.get(0);
+		Vector3f pivotInA = new Vector3f();
+		rbA.getCenterOfMassPosition(pivotInA);
+		
+		RigidBody rbB = bodies.get(1);
+		Vector3f pivotInB = new Vector3f();
+		rbB.getCenterOfMassPosition(pivotInB);
+		
+		Point2PointConstraint joint = new Point2PointConstraint(rbA, rbB, pivotInA, pivotInB);*/
+		//dynamicsWorld.addConstraint(joint, true);
+		
+		//selectedBody = bodies.get(1);
+		//dynamicsWorld.addConstraint(joint, true);
+	}
+	
+	private RigidBody createBody(float start_x, float start_y, float start_z) {
+		CollisionShape colShape = new SphereShape(1f);
+		collisionShapes.add(colShape);
+		
+		Transform startTransform = new Transform();
+		startTransform.setIdentity();
+
+		float mass = 10f;
+
+		// rigidbody is dynamic if and only if mass is non zero, otherwise static
+		boolean isDynamic = (mass != 0f);
+
+		Vector3f localInertia = new Vector3f(0, 0, 0);
+		if (isDynamic) {
+			colShape.calculateLocalInertia(mass, localInertia);
+		}
+
+		DefaultMotionState myMotionState = new DefaultMotionState(startTransform);
+		RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, colShape, localInertia);
+
+		RigidBody body = new RigidBody(rbInfo);
+		//Elastic Material
+		body.setFriction(1.5f);
+		body.setRestitution(1.3f);
+		body.setActivationState(RigidBody.ISLAND_SLEEPING);
+
+		dynamicsWorld.addRigidBody(body);
+		
+		return body;
+	}
+
+	private void createGround(CollisionShape groundShape,
+			Transform groundTransform) {
+		float mass = 0f;
+
+		// rigidbody is dynamic if and only if mass is non zero, otherwise static
+		boolean isDynamic = (mass != 0f);
+
+		Vector3f localInertia = new Vector3f(0, 0, 0);
+		if (isDynamic) {
+			groundShape.calculateLocalInertia(mass, localInertia);
+		}
+
+		// using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		DefaultMotionState myMotionState = new DefaultMotionState(groundTransform);
+		RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, groundShape, localInertia);
+		RigidBody body = new RigidBody(rbInfo);
+
+		//Elastic Ground
+		body.setFriction(0.5f);
+		body.setRestitution(0.5f);
+
+		// add the body to the dynamics world
+		dynamicsWorld.addRigidBody(body);
+	}
+
+	@Override
+	public GUIEvent updateKeyboard(KeyEvent event) {
+		if(event.isAnyKeyDown(KeyEvent.TSK_SPACE)) {
+			pressed = true;
+		} else if(event.isAnyKeyUp(KeyEvent.TSK_SPACE)) {
+			pressed = false;
+		}
+
+		return null;
+	}
+
+}
