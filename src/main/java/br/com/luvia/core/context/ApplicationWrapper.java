5c116fee604c5469e59aecc78c14cc5947a49337
yuripourre <yuriamayuri@gmail.com>
2015-11-01 23:41:36 -0500

    Add ApplicationWrapper sketch

---
+++ src/main/java/br/com/luvia/core/context/ApplicationWrapper.java
@@ -0,0 +1,32 @@
+package br.com.luvia.core.context;
+
+import br.com.etyllica.core.context.Application;
+import br.com.etyllica.core.context.Context;
+import br.com.etyllica.core.graphics.Graphic;
+import br.com.luvia.core.G3DEventListener;
+
+public abstract class ApplicationWrapper extends ApplicationGL implements G3DEventListener {
+	
+	protected Application application;
+	
+	public ApplicationWrapper(Application application) {
+		super(application.getX(), application.getY(), application.getW(), application.getH());
+		this.application = application;
+		init();
+	}
+	
+	private void init() {
+		nextApplication = this;
+		clearBeforeDraw = true;
+	}
+	
+	@Override
+	public void draw(Graphic g) {
+		application.draw(g);
+	}
+	
+	@Override
+	public Context getNextApplication() {
+		return application.getNextApplication();
+	}
+}
