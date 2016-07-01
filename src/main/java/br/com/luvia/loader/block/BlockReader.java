f17a0f686ea4a4539cebfd860c40775e2fb27b2d
Yuri Pourre <yuriamayuri@gmail.com>
2016-07-01 19:44:19 -0300

    Remove block loader

--- src/main/java/br/com/luvia/loader/block/BlockReader.java
+++
@@ -1,9 +0,0 @@
-package br.com.luvia.loader.block;
-
-import java.io.IOException;
-
-import br.com.luvia.graphics.Block;
-
-public interface BlockReader {
-	public Block loadBlock(String path) throws IOException;
-}
--- src/main/java/br/com/luvia/loader/block/BlockWriter.java
+++
@@ -1,9 +0,0 @@
-package br.com.luvia.loader.block;
-
-import java.io.IOException;
-
-import br.com.luvia.graphics.Block;
-
-public interface BlockWriter {
-	public void saveBlock(Block block, String path) throws IOException;
-}
